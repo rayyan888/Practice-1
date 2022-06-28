f869c60a8eae02de2c0317c5ba85752081e04ce3
rayyan888 <pss.22388.rayyan@gmail.com>
2022-06-28 15:15:49 +0500

    Clone OLX

---
+++ app.js
@@ -0,0 +1,35 @@
+//alert("Hello world");
+//var a = 5;
+//var b = 10;
+//var c = a + b;
+//alert(c)
+//var a = 5;
+//var b = 10;
+//var c = a + b;
+//console.log(c)
+
+//var a = 3;
+//var b = 5;
+//var c = 7;
+//var d = (b - a) * c;
+//alert(d);
+
+var age = +prompt("enter your age");
+if (age == 14) {
+
+    alert("your age is equal")
+}
+else if (age < 14) {
+    alert("your age is less")
+}
+else {
+    alert("your age is more")
+}
+var age = +prompt("enter your age");
+var gender=prompt("enter your gender")
+if (age >18 && gender==male){
+    alert("you are allow to write")
+}
+else{
+    alert("You are not  allowed")
+}
